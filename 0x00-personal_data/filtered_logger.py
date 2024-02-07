d991eafcac37992e59dbe295d661068ef3c37890
DD1C7470R <gracevalley101@gmail.com>
2024-02-07 20:43:22 +0000

    Add formatting class

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -15,3 +15,20 @@ def filter_datum(
     for field in fields:
         message = re.sub(pattern.format(field, separator), redaction, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """to filter values in incoming log records using filter_datum"""
+        msg = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
