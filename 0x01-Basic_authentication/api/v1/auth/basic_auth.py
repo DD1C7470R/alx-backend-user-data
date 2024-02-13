a9f1b39f7a1035f191bb12e64a5b2da153f25489
DD1C7470R <gracevalley101@gmail.com>
2024-02-13 17:01:45 +0000

    Add basic auth clss

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+    Defines BasicAuth class
+"""
+from auth import Auth
+from flask import request
+from typing import List, TypeVar
+
+
+class BasicAuth(Auth):
+    """Basic auth class to handle basic user authorization"""
+    pass
