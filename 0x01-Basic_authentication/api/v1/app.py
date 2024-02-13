bda55797154de44f19d2065aa29c774a2ebea72f
DD1C7470R <gracevalley101@gmail.com>
2024-02-13 16:17:11 +0000

    Fix excluded path variable declaration

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,6 +14,11 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
+excluded_paths = [
+    '/api/v1/status/',
+    '/api/v1/unauthorized/',
+    '/api/v1/forbidden/'
+]
 
 
 if os.getenv('AUTH_TYPE') == 'auth':
