fff552e197010520baf5a18f2288173c699ab187
DD1C7470R <gracevalley101@gmail.com>
2024-02-13 21:53:29 +0000

    Update basic auth class

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -57,3 +57,24 @@ class BasicAuth(Auth):
                 return None
             else:
                 return None
+
+    def user_object_from_credentials(
+        self, user_email: str, user_pwd: str
+    ) -> TypeVar('User'):
+        """returns the User instance based on his email and password.
+        """
+        if user_email is None or user_pwd is None:
+            return None
+        elif not isinstance(user_email, str) or not isinstance(user_pwd, str):
+            return None
+        else:
+            found_user = None
+            users = [user.to_json() for user in User.all()]
+            for user in users:
+                if user['email'] == user_email:
+                    user = User.search({"email": user_email})[0]
+                    if user.is_valid_password(user_pwd):
+                        return user
+                else:
+                    return None
+            return None
