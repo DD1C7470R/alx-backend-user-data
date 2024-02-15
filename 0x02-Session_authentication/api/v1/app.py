d6f678809e5aa35706ba0b4aef96c73b63aee2e8
DD1C7470R <gracevalley101@gmail.com>
2024-02-15 21:47:20 +0000

    Add login route to excluded path for auth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -55,6 +55,7 @@ def authenticate_user():
             '/api/v1/status/',
             '/api/v1/unauthorized/',
             '/api/v1/forbidden/',
+            '/api/v1/auth_session/login'
         ]
     if auth.require_auth(request.path, excluded_paths):
         is_cookie_set = request.cookies.get(getenv("SESSION_NAME"))
