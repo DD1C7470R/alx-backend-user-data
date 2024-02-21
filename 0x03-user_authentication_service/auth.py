d225bcac4da6a43c7af5b440d00af6e3156c2af6
DD1C7470R <gracevalley101@gmail.com>
2024-02-21 21:02:08 +0000

    Hot fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -110,14 +110,17 @@ class Auth:
         """generate a UUID and update the user’s
         reset_token database field. Return the token.
         """
-        if email is None or not isinstance(email, str):
-            return None
-        user = self._db.find_user_by(email=email)
-        if not isinstance(user, User):
-            raise ValueError
-        reset_token = _generate_uuid()
-        self._db.update_user(user.id, reset_token=reset_token)
-        return reset_token
+        try:
+            if e§mail is None or not isinstance(email, str):
+                return None
+            user = self._db.find_user_by(email=email)
+            if not isinstance(user, User):
+                raise ValueError
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        except Exception as e:
+            raise e
 
     def update_password(self, reset_token: str, password: str) -> str:
         """generate a new password for the user.
